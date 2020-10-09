f3f6fad37c475944987d30b9913596d2c121a2e6
qa-apprenticeships <andrew.burgess@qa.com>
2020-10-09 16:41:24 +0100

    Initial version

---
+++ Java/6.2 - Behaviour driven development (BDD)/src/main/java/com/qa/core/Calculator.java
@@ -0,0 +1,17 @@
+package com.qa.core;
+
+public class Calculator {
+	private int sum;
+
+	public Calculator() {
+		this.sum = 0;
+	}
+
+	public void addTwoNumber(int x, int y) {
+		sum = x + y;
+	}
+
+	public int getresult() {
+		return this.sum;
+	}
+}
---
+++ Java/8 - Coding standards/ReadMe.txt
@@ -0,0 +1,4 @@
+Chapter 8 - Coding Standards and Practice
+-----------------------------------------
+
+There is no starter project necessary for this lab.
